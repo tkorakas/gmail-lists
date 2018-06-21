1c10b87b03bef82e3b0e17efde5aae0306eee56a
Thanos Korakas <thanoskorakas@gmail.com>
2018-06-21 22:46:46 +0300

    Update pulblish.js

--- publish/pulblish.js
+++ publish/pulblish.js
@@ -4,7 +4,6 @@ const uploadOptions = require('./webstore_credentials');
 webstore_upload(uploadOptions)
   .then(function (result) {
     console.log(result);
-    // do somethings nice
     return 'yay';
   })
   .catch(function (err) {
